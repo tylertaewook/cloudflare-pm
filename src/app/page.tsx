7d572b6fd445372acc1c7c67102b9706e6c83b44
Tyler Kim <tk2891@columbia.edu>
2026-01-21 15:31:14 +0900

    installed tailwindcss

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
 			<main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
 				<Image className="dark:invert" src="/next.svg" alt="Next.js logo" width={180} height={38} priority />
 				<ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-					<li className="mb-2 tracking-[-.01em]">
+					<li className="mb-2 tracking-[-.01em] text-red-500">
 						Get started by editing{" "}
 						<code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
 							src/app/page.tsx
